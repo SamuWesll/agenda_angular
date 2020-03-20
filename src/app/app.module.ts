3b26c766ceadb930367defdea0626be13bdf6ddf
Treinamento <Treinamento@RD723896.raiadrogasil.com>
2020-03-20 10:26:29 -0300

    parte 8 - adicionando modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA, Component, ViewChild } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule, ModalDirective } from 'angular-bootstrap-md';
 import { EventComponent } from './components/event/event.component';
 
 
